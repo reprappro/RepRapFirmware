9173bd4ae9bdc3b7650c3067feb3b4b59e775174
Jean-Marc Giacalone <jmgiacalone@hotmail.com>
2013-09-12 13:13:57 +0100

    remove Release folder from repo.

--- Release/arduino/RingBuffer.cpp.d
+++
@@ -1,5 +0,0 @@
-arduino/RingBuffer.cpp.d arduino/RingBuffer.cpp.o: \
- /usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/RingBuffer.cpp \
- /usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/RingBuffer.h
-
-/usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/RingBuffer.h:
--- Release/arduino/WMath.cpp.d
+++
@@ -1,5 +0,0 @@
-arduino/WMath.cpp.d arduino/WMath.cpp.o: \
- /usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/WMath.cpp \
- /usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/WMath.h
-
-/usr/local/arduino-1.5.2/hardware/arduino/sam/cores/arduino/WMath.h:
