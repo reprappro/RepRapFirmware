7e39cf094106759bfb4c5fc81a2568db182c57a3
zombiepantslol <bmasterc@gmail.com>
2014-09-16 14:17:15 +0200

    Version 0.89k-zpl
    Added support for files generated by S3D

--- Configuration.h
+++ Configuration.h
@@ -24,8 +24,8 @@ Licence: GPL
 #define CONFIGURATION_H
 
 #define NAME "RepRapFirmware"
-#define VERSION "0.89j-zpl"
-#define DATE "2014-09-15"
+#define VERSION "0.89k-zpl"
+#define DATE "2014-09-16"
 #define AUTHORS "reprappro, dc42, zpl"
 
 // Other firmware that we might switch to be compatible with.
